79cc02a334265f426f3ee972fff27114a3e0041c
ShiYunHuang317 <betty860317@gmail.com>
2018-03-22 14:36:25 +0800

    說明

---
+++ 0322/t1.php
@@ -0,0 +1,16 @@
+<?php
+echo "^_^";
+
+if( !isset($_GET['aa'])
+	||!isset($_GET['bb'])
+	||!is_numeric($_GET['aa'])
+	||!is_numeric($_GET['bb'])){
+	echo"參數不正確";
+	exit;
+}
+
+
+$x = $_GET['aa'];
+$y = $_GET['bb'];
+$z = $x+$y;
+echo $z;
